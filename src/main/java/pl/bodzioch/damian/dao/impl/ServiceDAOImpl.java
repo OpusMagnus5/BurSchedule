9b78dd38519d48eeac72bf89e5e0cc47fbe34e71
OpusMagnus5 <damianbodzioch95@gmail.com>
2023-10-08 18:23:11 +0200

    service db

---
+++ src/main/java/pl/bodzioch/damian/dao/impl/ServiceDAOImpl.java
@@ -0,0 +1,10 @@
+package pl.bodzioch.damian.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import pl.bodzioch.damian.dao.ServiceDAO;
+
+@Repository
+public class ServiceDAOImpl implements ServiceDAO {
+
+
+}
---
+++ src/main/java/pl/bodzioch/damian/entity/ServiceDbEntity.java
@@ -0,0 +1,43 @@
+package pl.bodzioch.damian.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.generator.EventType;
+import pl.bodzioch.damian.configuration.database.GeneratedUuidValue;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "services")
+public class ServiceDbEntity {
+
+    @Id
+    @GeneratedUuidValue(types = EventType.INSERT)
+    private String id;
+    @Column(name = "bur_id")
+    private Long burId;
+    private String status;
+    private String title;
+    @Column(name = "date_beginning_of_service")
+    private LocalDate dateBeginningOfService;
+    @Column(name = "date_completed_of_service")
+    private LocalDate dateCompletionOfService;
+    @Column(name = "number_of_hours")
+    private Integer numberOfHours;
+    @Column(name = "service_provider_bur_id")
+    private Long serviceProviderId;
+    @Column(name = "service_provider_name")
+    private String serviceProviderName;
+    private String location;
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+    @Column(name = "modify_date")
+    @CurrentTimestamp(event = EventType.UPDATE)
+    private LocalDateTime modifyDate;
+}
