26b145eec5a76123ec9cb4939fb40e22c565e1e9
OpusMagnus5 <damianbodzioch95@gmail.com>
2023-11-07 18:21:08 +0100

    admin panel

---
+++ src/main/resources/page/templates/admin/admin-menu.js
@@ -0,0 +1,29 @@
+import { handleAddUserButton } from "../../admin/admin.js";
+import { getMessage } from "../../util/config.js";
+
+export function setMenu() {
+  document.querySelector(".menu .menu-service-list").textContent = getMessage("menu-service-list");
+  document.querySelector(".menu .menu-scheduler-from-file").textContent = getMessage("menu-scheduler-from-file");
+  document.querySelector(".menu .menu-scheduler-create").textContent = getMessage("menu-scheduler-create");
+  document.querySelector(".menu .menu-add-user").textContent = getMessage("menu-add-user");
+
+  document.querySelectorAll(".menu-service-list").forEach((element) => {
+    element.addEventListener("click", function () {
+      window.location.href = "services-list";
+    });
+  });
+
+  document.querySelectorAll(".menu-scheduler-from-file").forEach((element) => {
+    element.addEventListener("click", function () {
+      window.location.href = "scheduler-upload";
+    });
+  });
+
+  document.querySelectorAll(".menu-scheduler-create").forEach((element) => {
+    element.addEventListener("click", function () {
+      window.location.href = "scheduler-create";
+    });
+  });
+
+  document.querySelector(".menu-add-user").addEventListener("click", handleAddUserButton);
+}
